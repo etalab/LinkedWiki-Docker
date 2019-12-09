e3e3f25c0fa0d1d2611a28059a1b90a3e02dad36
Antoine D <antoine.desbordes@gmail.com>
2019-12-09 16:11:23 +0000

    change default endpoint to topo

--- extra-entrypoint-run-first.sh
+++ extra-entrypoint-run-first.sh
@@ -4,10 +4,10 @@ wfLoadExtension( "Push", "${DOLLAR}IP/extensions/LinkedWiki/bundle/Push/extensio
 wfLoadExtension( "NamespaceRelations", "${DOLLAR}IP/extensions/LinkedWiki/bundle/NamespaceRelations/extension.json" );
 
 ${DOLLAR}wgLinkedWikiOSMAccessToken = "pk.eyJ1IjoibC12aW5jZW50LWwiLCJhIjoiY2pzMWtlNG90MXA5cTQ5dGYwNDRyMDRvayJ9.RhYAa9O0Qla5zhJAb9iwJA";
-${DOLLAR}wgLinkedWikiConfigSPARQLServices["http://wdqs-proxy/bigdata/sparql"] = array(
+${DOLLAR}wgLinkedWikiConfigSPARQLServices["https://sparql.topo.transport.data.gouv.fr/bigdata/sparql"] = array(
 	"debug" => false,
 	"isReadOnly" => true,
-	"endpointRead" => "http://wdqs-proxy/bigdata/sparql",
+	"endpointRead" => "https://sparql.topo.transport.data.gouv.fr/bigdata/sparql",
 );
-${DOLLAR}wgLinkedWikiSPARQLServiceByDefault= "http://wdqs-proxy/bigdata/sparql";
+${DOLLAR}wgLinkedWikiSPARQLServiceByDefault= "https://sparql.topo.transport.data.gouv.fr/bigdata/sparql";
 ' >> /LocalSettings.php.template
